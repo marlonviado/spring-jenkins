ff560004ec08b52c8fb145486335fc6755b77d86
marlon viado <mfviado@hotmail.com>
2026-04-28 16:25:35 +0800

    Remove Slf4j logs

--- src/main/java/com/marlonviado/SpringJenkinsApplication.java
+++ src/main/java/com/marlonviado/SpringJenkinsApplication.java
@@ -3,16 +3,12 @@ package com.marlonviado;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import lombok.extern.slf4j.Slf4j;
-
-
 @SpringBootApplication
-@Slf4j
 public class SpringJenkinsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJenkinsApplication.class, args);
-		log.info("Application is running...");
+		System.out.println("Application is running...");
 	}
 
 }
