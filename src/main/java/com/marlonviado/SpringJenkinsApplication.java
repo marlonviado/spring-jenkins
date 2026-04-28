969a66c96390d95f7c0bb0dc5253b74eedf9f07a
marlon viado <mfviado@hotmail.com>
2026-04-28 15:46:37 +0800

    Add logs

--- src/main/java/com/marlonviado/SpringJenkinsApplication.java
+++ src/main/java/com/marlonviado/SpringJenkinsApplication.java
@@ -3,12 +3,16 @@ package com.marlonviado;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 
 @SpringBootApplication
+@Slf4j
 public class SpringJenkinsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJenkinsApplication.class, args);
+		log.info("Application is running...");
 	}
 
 }
