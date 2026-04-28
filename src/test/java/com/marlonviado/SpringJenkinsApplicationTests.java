eac658fe762c6bfb3d7f81876406bd7900db627f
marlon viado <mfviado@hotmail.com>
2026-04-28 15:15:08 +0800

    First commit

---
+++ src/test/java/com/marlonviado/SpringJenkinsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.marlonviado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootTest
+@Slf4j
+class SpringJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+		log.info("Test Application Running!!!!");
+	}
+
+}
